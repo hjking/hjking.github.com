ac815f3f41d4c32a7055e48aa5b79d045b30d43d
kinghom <bestkindy@gmail.com>
2013-09-14 14:46:19 +0800

    重新组织js和css文件

--- js/all.js
+++ js/all.js
@@ -2,6 +2,6 @@
 ---
 {% include js/jquery.min.js %}
 {% include js/underscore.min.js %}
-{% include js/bootstrap/bootstrap.min.js %}
+{% include js/calendar.js %}
 {% include js/jquery.githubwidget.min.js %}
 {% include js/custom.js %}
